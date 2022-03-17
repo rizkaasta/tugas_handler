84dfef8f267d886584abcbd464d7c7dc66ee0830
rizkaasta <rizkaasta98@gmail.com>
2022-03-17 19:28:39 +0700

    handler

--- handler.go
+++ handler.go
@@ -10,12 +10,25 @@ import (
 func Product(c *gin.Context) {
 	judul := c.Query("judul")
 	harga := c.Query("harga")
-	time := time.Now()
+	time := time.RFC850
 
 	c.JSON(http.StatusOK, gin.H{
 		"Message" : "item buku",
 		"Item Name" : judul,
 		"Time" : time,
 		"Price" : harga,
+	})
+}
+
+func IdProduct( c*gin.Context) {
+	id := c.Param("id")
+	name := c.Query("name")
+	price := c.Query("price")
+
+	c.JSON(http.StatusOK, gin.H{
+		"id" : id,
+		"Message" : "item name",
+		"Product name" : name,
+		"Price" : price,
 	})
 }
