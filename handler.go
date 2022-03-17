29d6b1cb3d4c002fa8e699641d76da141af15f60
rizkaasta <rizkaasta98@gmail.com>
2022-03-17 18:59:41 +0700

    handler

--- handler.go
+++ handler.go
@@ -1,3 +1,21 @@
 package handler
 
-import 
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Product(c *gin.Context) {
+	judul := c.Query("judul")
+	harga := c.Query("harga")
+	time := time.Now()
+
+	c.JSON(http.StatusOK, gin.H{
+		"Message" : "item buku",
+		"Item Name" : judul,
+		"Time" : time,
+		"Price" : harga,
+	})
+}
